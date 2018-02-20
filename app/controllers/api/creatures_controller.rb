2120d22e8b76d3a656957886f24b73b9ea1d8fbf
dcamriff <driffelatl@gmail.com>
2018-02-20 09:50:56 -0500

    Set up rails app and added Api creatures controller.

---
+++ app/controllers/api/creatures_controller.rb
@@ -0,0 +1,7 @@
+class Api::CreaturesController < ApplicationController
+    def index
+        @creatures = Creature.all
+
+        render json: @creatures
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  namespace :api do
+    get "/creatures", to: "creatures#index", as: "creatures"
+  end
+end
